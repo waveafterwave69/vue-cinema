57cf6c1ea9334aa8c8bbaf69302eda233dc33cff
Michael Shonov <waveafterwave3w2@gmail.com>
2026-04-01 14:49:29 +0300

    refactor: wave-hooks

--- src/composables/useGetRandomMovie.ts
+++ src/composables/useGetRandomMovie.ts
@@ -2,35 +2,44 @@ import { moviesApi } from '@/services/movies'
 import { collections } from '@/static/static'
 import type { Movie } from '@/types/movies'
 import { ref } from 'vue'
+import { useRandomNumber } from 'wave-hooks'
 
 export const useGetRandomMovie = () => {
     const movie = ref<Movie>()
     const error = ref<string | null>(null)
 
-    const randomNumberOne = Math.floor(Math.random() * 35) + 1
-    const randomNumberTwo = Math.floor(Math.random() * 20)
-    const randomNumberThree = Math.floor(Math.random() * collections.length)
+    const { randomNumber: page, refresh: refreshPage } = useRandomNumber(1, 35)
+    const { randomNumber: itemIndex, refresh: refreshItemIndex } = useRandomNumber(0, 19)
+    const { randomNumber: collectionIndex, refresh: refreshCollectionIndex } = useRandomNumber(
+        0,
+        collections.length - 1,
+    )
 
     const getMovies = async () => {
         try {
+            refreshPage()
+            refreshItemIndex()
+            refreshCollectionIndex()
+
             const movieData = await moviesApi.getMoviesCategory(
-                collections[randomNumberThree]!,
-                randomNumberOne,
+                collections[collectionIndex.value]!,
+                page.value,
             )
 
-            if (movieData.items && movieData.items[randomNumberTwo]) {
-                movie.value = movieData.items[randomNumberTwo]
+            if (movieData.items && movieData.items[itemIndex.value]) {
+                movie.value = movieData.items[itemIndex.value]
             } else {
                 movie.value = movieData.items[0]
             }
         } catch (err) {
             error.value = 'Не удалось загрузить фильм.'
-            console.log(err)
+            console.error(err)
         }
     }
 
     return {
         movie,
+        error,
         getMovies,
     }
 }
