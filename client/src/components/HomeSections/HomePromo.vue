f81234ec0de59bf775097776754aacaa67354aac
Michael Shonov <waveafterwave3w2@gmail.com>
2026-05-20 22:36:57 +0300

    fix(promo): text

--- client/src/components/HomeSections/HomePromo.vue
+++ client/src/components/HomeSections/HomePromo.vue
@@ -109,7 +109,7 @@ onMounted(loadRandomMovie)
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M8 5v14l11-7z" />
                     </svg>
-                    Смотреть
+                    О фильме
                 </button>
             </div>
         </div>
